1e222d0eab43f460e36a54d72cf4f868e9eafe38
Kevin Dexter <kevin.dexter@comcast.net>
2018-04-13 15:11:18 -0500

    Allow user to add creatures

--- script.js
+++ script.js
@@ -5,11 +5,56 @@ $(document).ready(function () {
 
 console.log('script.js has been loaded');
 
+let count = 0;
+
 function onReady() {
-  $('#incrementingButton').on('click', function() {
-    let buttonVal = $('#buttonValue');
-    let newVal = buttonVal.text()*1+1;
-    $('#buttonValue').text(newVal);
-    console.log(newVal);
-  });
+  // $('body').append('<button id="incrementingButton">Increment!!!</button>');
+  // $('body').append('<span id="buttonValue">' + count + '</span>');
+
+  $('#incrementingButton').on('click', clickHandler);
+  $('#createCreatureButton').on('click', addSeaCreature);
+}
+
+function clickHandler() {
+  console.log('counter button was clicked');
+  count ++;
+  console.log(count);
+  updateDomCount();
+}
+
+function updateDomCount() {
+  $('#buttonValue').text(count);
+}
+
+function getName () {
+  return $('#seaCreatureName').val();
+}
+
+function getSize () {
+  return $('#seaCreatureSize').val();
+}
+
+function getType () {
+  return $('#seaCreatureType').val();
+}
+
+function getSpeed () {
+  return $('#seaCreatureSpeed').val();
+}
+
+function clearInputBoxes() {
+  $('#seaCreatureName').val('');
+  $('#seaCreatureSize').val('');
+  $('#seaCreatureType').val('');
+  $('#seaCreatureSpeed').val('');
+}
+
+function addSeaCreature () {
+  let name = '<td>' + getName() + '</td>';
+  let size = '<td>' + getSize() + '</td>';
+  let type = '<td>' + getType() + '</td>';
+  let speed = '<td>' + getSpeed() + '</td>';
+  let newRow = '<tr>' + name + size + type + speed + '</tr>'
+  $('#seaCreatureTable').prepend(newRow);
+  clearInputBoxes();
 }
