ba8a7aa3136fdd48d5014948c87cee27456339a2
Kevin Dexter <kevin.dexter@comcast.net>
2018-04-13 11:44:10 -0500

    some added functionality/style

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 $(document).ready(function () {
-  console.log('ready');
+  console.log('jQuery has been loaded');
+  onReady();
 });
+
+console.log('script.js has been loaded');
+
+function onReady() {
+  $('#incrementingButton').on('click', function() {
+    let buttonVal = $('#buttonValue');
+    let newVal = buttonVal.text()*1+1;
+    $('#buttonValue').text(newVal);
+    console.log(newVal);
+  });
+}
